ab0e91b28852df00f64f7b2db793354439b2f8f2
thriggle <thriggle@live.com>
2022-03-31 09:48:12 -0500

    Forbidden word tweak

--- NameGenerator.js
+++ NameGenerator.js
@@ -81,33 +81,32 @@ function NameGenerator(sourceJson,callback,forbiddenWords,randomizer){
     if(randomizer){
         MathRandom = randomizer;
     }else{
-        MathRandom = pseudoRandomNumberGenerator(Math.random().toString());
+        MathRandom = pseudoRandomNumberGenerator((Math.random()*1000000).toString());
     }    
     function pseudoRandomNumberGenerator(word){
+        function xmur3(str) {
+            for(var i = 0, h = 1779033703 ^ str.length; i < str.length; i++) {
+                h = Math.imul(h ^ str.charCodeAt(i), 3432918353);
+                h = h << 13 | h >>> 19;
+            } return function() {
+                h = Math.imul(h ^ (h >>> 16), 2246822507);
+                h = Math.imul(h ^ (h >>> 13), 3266489909);
+                return (h ^= h >>> 16) >>> 0;
+            }
+        }
+        function xoshiro128ss(a, b, c, d) {
+            return function() {
+                var t = b << 9, r = a * 5; r = (r << 7 | r >>> 25) * 9;
+                c ^= a; d ^= b;
+                b ^= c; a ^= d; c ^= t;
+                d = d << 11 | d >>> 21;
+                return (r >>> 0) / 4294967296;
+            }
+        }
         var seed = xmur3(word);
         return xoshiro128ss(seed(), seed(), seed(), seed());
     }
-    function xmur3(str) {
-        for(var i = 0, h = 1779033703 ^ str.length; i < str.length; i++) {
-            h = Math.imul(h ^ str.charCodeAt(i), 3432918353);
-            h = h << 13 | h >>> 19;
-        } return function() {
-            h = Math.imul(h ^ (h >>> 16), 2246822507);
-            h = Math.imul(h ^ (h >>> 13), 3266489909);
-            return (h ^= h >>> 16) >>> 0;
-        }
-    }
-    function xoshiro128ss(a, b, c, d) {
-        return function() {
-            var t = b << 9, r = a * 5; r = (r << 7 | r >>> 25) * 9;
-            c ^= a; d ^= b;
-            b ^= c; a ^= d; c ^= t;
-            d = d << 11 | d >>> 21;
-            return (r >>> 0) / 4294967296;
-        }
-    }
-    var forbiddenWords = forbiddenWords;
-    if(typeof forbiddenWords === "undefined"){
+    if(typeof forbiddenWords === "undefined" || forbiddenWords === null){
         forbiddenWords = ["anus","ass"];
     }
     this.getRandomName = getRandomName;
