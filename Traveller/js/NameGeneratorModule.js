1ab1e4ae2b8a0d5191dbc309318d05cfcc179f14
thriggle <thriggle@live.com>
2023-10-19 23:33:28 -0500

    better error handling

--- Traveller/js/NameGeneratorModule.js
+++ Traveller/js/NameGeneratorModule.js
@@ -1,80 +1,3 @@
-(function (global) {
-    if (typeof global.JSON == "undefined" || !global.JSON) {
-        global.JSON = {};
-    }
-    if(!global.JSON.minify){
-        global.JSON.minify = function JSON_minify(json) {
-
-            var tokenizer = /"|(\/\*)|(\*\/)|(\/\/)|\n|\r/g,
-                in_string = false,
-                in_multiline_comment = false,
-                in_singleline_comment = false,
-                tmp, tmp2, new_str = [], ns = 0, from = 0, lc, rc,
-                prevFrom
-                ;
-    
-            tokenizer.lastIndex = 0;
-    
-            while (tmp = tokenizer.exec(json)) {
-                lc = RegExp.leftContext;
-                rc = RegExp.rightContext;
-                if (!in_multiline_comment && !in_singleline_comment) {
-                    tmp2 = lc.substring(from);
-                    if (!in_string) {
-                        tmp2 = tmp2.replace(/(\n|\r|\s)+/g, "");
-                    }
-                    new_str[ns++] = tmp2;
-                }
-                prevFrom = from;
-                from = tokenizer.lastIndex;
-    
-                // found a " character, and we're not currently in
-                // a comment? check for previous `\` escaping immediately
-                // leftward adjacent to this match
-                if (tmp[0] == "\"" && !in_multiline_comment && !in_singleline_comment) {
-                    // perform look-behind escaping match, but
-                    // limit left-context matching to only go back
-                    // to the position of the last token match
-                    //
-                    // see: https://github.com/getify/JSON.minify/issues/64
-                    tmp2 = lc.substring(prevFrom).match(/\\+$/);
-    
-                    // start of string with ", or unescaped " character found to end string?
-                    if (!in_string || !tmp2 || (tmp2[0].length % 2) == 0) {
-                        in_string = !in_string;
-                    }
-                    from--; // include " character in next catch
-                    rc = json.substring(from);
-                }
-                else if (tmp[0] == "/*" && !in_string && !in_multiline_comment && !in_singleline_comment) {
-                    in_multiline_comment = true;
-                }
-                else if (tmp[0] == "*/" && !in_string && in_multiline_comment && !in_singleline_comment) {
-                    in_multiline_comment = false;
-                }
-                else if (tmp[0] == "//" && !in_string && !in_multiline_comment && !in_singleline_comment) {
-                    in_singleline_comment = true;
-                }
-                else if ((tmp[0] == "\n" || tmp[0] == "\r") && !in_string && !in_multiline_comment && in_singleline_comment) {
-                    in_singleline_comment = false;
-                }
-                else if (!in_multiline_comment && !in_singleline_comment && !(/\n|\r|\s/.test(tmp[0]))) {
-                    new_str[ns++] = tmp[0];
-                }
-            }
-            new_str[ns++] = rc;
-            return new_str.join("");
-        };
-    }    
-})(
-    // attempt to reference the global object
-    typeof globalThis != "undefined" ? globalThis :
-        typeof global != "undefined" ? global :
-            typeof window != "undefined" ? window :
-                typeof self != "undefined" ? self :
-                    typeof this != "undefined" ? this :
-                        {}
-);
 var self = {};
 export function NameGenerator(sourceJson,callback,forbiddenWords,randomizer,fromObject){
     if(typeof fromObject == "undefined"){
@@ -175,6 +98,7 @@ export function NameGenerator(sourceJson,callback,forbiddenWords,randomizer,from
             extraLogs = true;
         }
         var templatesSubset = self.templates[key];
+        if(typeof templatesSubset === "undefined"){return "Invalid Key='"+key + "'";}
         var templateRoll =(templatesSubset.length * MathRandom()) >>> 0 ;
         var template = templatesSubset[templateRoll];
         var phrases = self.unpackStringTemplate(template); // convert string into array of phrase objects with child segments
--- netlify/functions/name.mjs
+++ netlify/functions/name.mjs
@@ -4,35 +4,43 @@ import { getNames } from "../../Traveller/js/namesModule";
 export default async (req, context) => {
     try{
         return new Promise(async (resolve,reject) => {
-            const { key } = context.params;
-            const urls = req.url.split("/");
-            const url = urls[urls.length-1];
-            const generator = await NameGeneratorPromise();
-            var params = {};
-            if(url.indexOf("?") >= 0){
-                var search = url.split("?");
-                params.key = search[0];
-                if(search.length > 1){
-                    var kvp = search[1].split("&");
-                    for(var i = 0, len = kvp.length; i < len; i++){
-                        var pair = kvp[i].split("=");
-                        pair[0] = pair[0].replace("$","");
-                        params[pair[0]] = pair.length > 1 ? pair[1] : true;
+            try{
+                const { key } = context.params;
+                const urls = req.url.split("/");
+                const url = urls[urls.length-1];
+                const generator = await NameGeneratorPromise();
+                var params = {};
+                if(url.indexOf("?") >= 0){
+                    var search = url.split("?");
+                    params.key = search[0];
+                    if(search.length > 1){
+                        var kvp = search[1].split("&");
+                        for(var i = 0, len = kvp.length; i < len; i++){
+                            var pair = kvp[i].split("=");
+                            pair[0] = pair[0].replace("$","");
+                            params[pair[0]] = pair.length > 1 ? pair[1] : true;
+                        }
                     }
+                }else{
+                    params.key = key;
                 }
-            }else{
-                params.key = key;
+                if(typeof params.key === "undefined"){params.key = "human";}
+                if(typeof params.top === "undefined"){params.top = 100;}else if(params.top < 0){params.top = 1;}else if(params.top > 1280){ params.top = 1280; }
+                console.log(params);
+                console.log(context.geo);
+                var names = [];
+
+                    for(var i = 0, len = params.top; i < len; i++){
+                        names.push(addCaps(generator.getRandomName(params.key).trim()));
+                    }
+                
+                var response = new Response(JSON.stringify({"results":names}),{"status":200, "statusText":names[0], headers:{"Content-Type":"application/json; charset=utf-8"}});
+                resolve(response);
+            }catch(error){
+                var response = new Response(JSON.stringify({"error":"invalid request"}),{"status":400,"headers":{"Content-Type":"application/json; charset=utf-8"}});
+                reject(response);
             }
-            if(typeof params.key === "undefined"){params.key = "human";}
-            if(typeof params.top === "undefined"){params.top = 100;}else if(params.top < 0){params.top = 1;}else if(params.top > 1280){ params.top = 1280; }
-            console.log(params);
-            var names = [];
-            for(var i = 0, len = params.top; i < len; i++){
-                names.push(addCaps(generator.getRandomName(params.key).trim()));
-            }
-            var response = new Response(JSON.stringify({"results":names}),{"status":200, "statusText":names[0], headers:{"Content-Type":"application/json; charset=utf-8"}});
-            resolve(response);
-        })
+        });
     }catch(error){
         return new Response(error);
     }
